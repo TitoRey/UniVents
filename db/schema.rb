cd02a8be5e9e1f08ddfae7b6531785159080ef55
Ghoulsimi07 <92327055+Ghoulsimi07@users.noreply.github.com>
2023-03-05 13:49:33 -0500

    Created Locations Table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_23_014803) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_27_075237) do
+  create_table "locations", force: :cascade do |t|
+    t.integer "location_id", null: false
+    t.datetime "remember_created_at"
+    t.datetime "remember_updated_at"
+    t.string "street_address"
+    t.integer "zipcode"
+    t.string "county"
+    t.string "city"
+    t.string "coordinates"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
