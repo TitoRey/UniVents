b2d5217276053621ab51c699009535c55bad1c1e
Lam Phan <lam.tphan129@gmail.com>
2023-03-07 20:17:36 -0500

    UN-13 Update Registration Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  get 'users/index'
+  devise_for :users, :controllers => { registrations: 'users/registrations' }
   root to: "home#index" 
 end
