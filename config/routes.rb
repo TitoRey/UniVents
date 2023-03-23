cd90d2927cf5b9f3be51ebf5e925225950a8aa8c
Tito <titorr2001@gmail.com>
2023-03-23 17:07:51 -0400

    Generating MVC for Events

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  # What is this for again? 
   get 'users/index'
+
+  # Lam might need to explain how this works
   devise_for :users, :controllers => { registrations: 'users/registrations' }
+  
+  # ToDo: Change the Root of the application. Decide w/ Team
   root to: "home#index" 
+  
+  resources :events
 end
