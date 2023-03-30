0920843f40d44803aa9bbbe50b179234f8dfcea2
combustibletoast <robert.sobert24@gmail.com>
2023-03-30 19:44:00 -0400

    Added is_admin function to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,24 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def is_admin
+    #Sorry about all of the comments, trying to figure this out. -Ena
+    
+    #https://www.freecodecamp.org/news/understanding-the-basics-of-ruby-on-rails-sql-databases-and-how-they-work-7a628cd42073/
+    #i think i do something like this with sql?:
+    #find userid where admin
+    #User.all gets a collection of all users
+    #From https://guides.rubyonrails.org/active_record_basics.html:
+    #users = User.where(name: 'David', occupation: 'Code Artist').order(created_at: :desc)
+    #changed in this case to
+    #user_is_admin = User.where(id: <this user's id>, admin: true)
+    #return self.instance_variable_get(@admin)
+
+    #Somewhere else in the code it will call u.is_admin where u is an instance of User
+    #Since u is an instance of an object, this method is in the context of an instance, so we can just access the field
+    #Additionally, DB records <=> Model objects. When you get a record you can manipulate it like an object in the model
+    #self. is equivalent to this. from java
+    return self.admin
+  end
 end
