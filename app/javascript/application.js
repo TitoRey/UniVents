d7277239bd04d9c7988c5101f630d9f0d225f728
Tito <titorr2001@gmail.com>
2023-04-16 18:45:09 -0400

    JS now works

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,4 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import "datatables"
-import "popper"
-import "bootstrap"
+import "./datatables/datatable"
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,5 +1,4 @@
 # Pin npm packages by running ./bin/importmap
-
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "https://ga.jspm.io/npm:@hotwired/stimulus@3.2.1/dist/stimulus.js"
@@ -10,8 +9,3 @@ pin 'jquery', to: "https://ga.jspm.io/npm:jquery@3.6.1/dist/jquery.js"
 pin "stimulus-dropdown", to: "https://ga.jspm.io/npm:stimulus-dropdown@2.1.0/dist/stimulus-dropdown.mjs"
 pin "hotkeys-js", to: "https://ga.jspm.io/npm:hotkeys-js@3.10.1/dist/hotkeys.esm.js"
 pin "stimulus-use", to: "https://ga.jspm.io/npm:stimulus-use@0.51.3/dist/index.js"
-pin "bootstrap", to: 'boostrap.min.js', preload: true
-pin "popper", to: 'popper.js', preload: true
-pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
-pin "datatables", to: "https://ga.jspm.io/npm:datatables@1.10.18/media/js/jquery.dataTables.js"
-pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.6.4/dist/jquery.js"
