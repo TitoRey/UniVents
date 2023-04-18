d906330ac18533b481e04968d4720f963cc4935e
Tito <titorr2001@gmail.com>
2023-04-18 14:50:24 -0400

    changing imports for importmaps

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import "./datatables/datatable"
+import "datatables/datatable"
+import "use_jquery"
