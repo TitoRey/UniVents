a152fa0751e155909a7d331d93a180637d72503a
Ghoulsimi07 <92327055+Ghoulsimi07@users.noreply.github.com>
2023-05-05 16:04:09 -0400

    Trying to figure out unit testing, gives exception everytime

--- test/models/location_test.rb
+++ test/models/location_test.rb
@@ -4,28 +4,27 @@ class LocationTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
-  test "valid location" do
-    
+ test "valid location" do
+  location = Location.new(:street_address => "4400 University Drive",:zipcode=> "22030",:country=> "USA",:county=> "Fairfax",:event_id => 10)
+  # Should assert creation successful
   end
   test "missing field" do
-    
+    assert_raise(Exception) {
+    location = Location.new(:street_address => "4400 University Drive",:zipcode=> "22030",:country=> "USA",:county=> "Fairfax")
+    location.create
+    # Missing coordinates -> Should return exception
+  }
   end
-  test "in zipcode" do
-    
+  
+  test "destroy location" do
+    location = Location.new(:street_address => "4400 University Drive",:zipcode=> "22030",:country=> "USA",:county=> "Fairfax")
+    location.create
+    location.destroy
   end
-  test "coordinate to location" do
-    
-  end
-  test "invalid coordinate to location" do
-    
-  end
-  test "location to coordinate" do
-    
-  end
-  test "invalid location to coordinate" do
-    
-  end
-  test "delete location" do
-    
+  
+  test "string location" do
+    location = Location.new(:street_address => "4400 University Drive",:zipcode=> "22030",:country=> "USA",:county=> "Fairfax")
+    location.create
+    assert location.full_address=="4400 University Drive, Farifax, 22030"
   end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,86 +6,55 @@ class UserTest < ActiveSupport::TestCase
   # end
 
   test "valid registration" do
-    
+    user = User.new(:password=>"ABCSDS231",:email=>"ssekar+00000@gmu.edu",:first_name=>"hello",:last_name=>"crazy",:password_confirmation=>"ABCSDS231",:admin=>false)
+    user.create
+    # Need to write assertion that user.create does not throw exceptions
   end
 
   test "inv reg missing field" do
-    user = User.new
-    assert_not user.save
+    assert_raise (Exception) {
+      user = User.new
+      user.create
+    } # no fields
   end
   test "inv reg weak password" do
-    user = User.new
-    user.password="hey"
-    user.email = "ssekar+0000@gmu.edu"
-    user.first_name="hello"
-    user.last_name="crazy"
-    user.password_confirmation="hey"
-    user.admin=false
-    user.user_id=1
-    assert_not user.save
+    assert_raise(Exception()) {
+      user = User.new(:password=>"ABCSDS231",:email=>"ssekar+00000@gmu.edu",:first_name=>"hello",:last_name=>"cry",:password_confirmation=>"ABCSDS231",:admin=>false)
+      user.create
+    }
   end
-  ''' test "inv reg wrong email" do
-    user=User.new
-    user.password="ABCSDS231"
-    user.email = "ssekar+0000@gmail.com"
-    user.first_name="hello"
-    user.last_name="crazy"
-    user.password_confirmation="ABCSDS231"
-    user.admin=false
-    user.user_id=1
-    assert_not user.save
+   test "inv reg wrong email" do
+    assert_raise(Exception()) {
+      user = User.new(:password=>"ABCSDS231",:email=>"ssekar+00000@gmail.com",:first_name=>"hello",:last_name=>"crazy",:password_confirmation=>"ABCSDS231",:admin=>false)
+   }
   end
   test "inv reg mismatch password" do
-    user=User.new
-    user.password="ABCSDS231"
-    user.email = "ssekar+0000@gmu.edu"
-    user.first_name="hello"
-    user.last_name="crazy"
-    user.password_confirmation="ABCSDS23232342341"
-    user.admin=false
-    user.user_id=1
-    assert_not user.save
+    assert_raise(Exception()) {
+      user=User.new
+      user = User.new(:password=>"ABCSDS231",:email=>"ssekar+00000@gmu.edu",:first_name=>"hello",:last_name=>"crazy",:password_confirmation=>"ABCSDS234234231",:admin=>false)
+    }
   end
   test "user exists" do
-    user=User.new
-    user.password="ABCSDS231"
-    user.email = "ssekar+0000@gmu.edu"
-    user.first_name="hello"
-    user.last_name="crazy"
-    user.password_confirmation="ABCSDS231"
-    user.admin=false
-    user.user_id=1
-    user.save
+    user = User.new(:password=>"ABCSDS231",:email=>"ssekar+00000@gmu.edu",:first_name=>"hello",:last_name=>"crazy",:password_confirmation=>"ABCSDS231",:admin=>false)
+    user.create
     user2 = User.find(email = "ssekar+0000.gmu.edu")
-    assert user2 == user
+    assert user==user2
   end
   test "valid login" do
-    user=User.new
-    user.password="ABCSDS231"
-    user.email = "ssekar+0000@gmu.edu"
-    user.first_name="hello"
-    user.last_name="crazy"
-    user.password_confirmation="ABCSDS231"
-    user.admin=false
-    user.user_id=1
-    user.save
+    user = User.new(:password=>"ABCSDS231",:email=>"ssekar+00000@gmu.edu",:first_name=>"hello",:last_name=>"crazy",:password_confirmation=>"ABCSDS231",:admin=>false)
+    user.create
     username = "ssekar+0000@gmu.edu"
     password = "ABCSDS231"
-    assert user.email = username and user.password = password
+    boolean = (user.email == username) and (user.password == password)
+    assert boolean
   end
   test "invalid login" do
-    user=User.new
-    user.password="ABCSDS231"
-    user.email = "ssekar+0000@gmu.edu"
-    user.first_name="hello"
-    user.last_name="crazy"
-    user.password_confirmation="ABCSDS231"
-    user.admin=false
-    user.user_id=1
-    user.save
+    user = User.new(:password=>"ABCSDS231",:email=>"ssekar+00000@gmu.edu",:first_name=>"hello",:last_name=>"crazy",:password_confirmation=>"ABCSDS231",:admin=>false)
+    user.create
     username = "ssekar+0000@gmu.edu"
     password = "ABCSDS2323421"
-    assert_not user.email = username and user.password = password
+    boolean = (user.email == username) and (user.password == password)
+    assert_not boolean
   end
   test "logged in" do
     
@@ -99,34 +68,17 @@ class UserTest < ActiveSupport::TestCase
     
   end
   test "is admin" do
-    user=User.new
-    user.password="ABCSDS231"
-    user.email = "ssekar+0000@gmu.edu"
-    user.first_name="hello"
-    user.last_name="crazy"
-    user.password_confirmation="ABCSDS231"
-    user.admin=true
-    user.user_id=1
-    user.save
+    user = User.new(:password=>"ABCSDS231",:email=>"ssekar+00000@gmu.edu",:first_name=>"hello",:last_name=>"crazy",:password_confirmation=>"ABCSDS231",:admin=>false)
+    user.create
     assert user.is_admin == true
   end
-  test "is active" do
-    user=User.new
-    user.password="ABCSDS231"
-    user.email = "ssekar+0000@gmu.edu"
-    user.first_name="hello"
-    user.last_name="crazy"
-    user.password_confirmation="ABCSDS231"
-    user.admin=false
-    user.user_id=1
-    user.active = true
-    user.save
-    assert user.active == true
-  end
+  
   test "delete user" do
-    
+    user = User.new(:password=>"ABCSDS231",:email=>"ssekar+00000@gmu.edu",:first_name=>"hello",:last_name=>"crazy",:password_confirmation=>"ABCSDS231",:admin=>false)
+    user.create
+    # Need to write assertion
   end
   test "update user" do
-    
-  end '''
+
+  end 
 end
