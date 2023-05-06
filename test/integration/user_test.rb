6dc837d65969baa4acdd6b7cdc0fd9d33e0a9116
Lam Phan <lam.tphan129@gmail.com>
2023-05-05 23:55:44 -0400

    One more test

--- test/integration/user_test.rb
+++ test/integration/user_test.rb
@@ -67,70 +67,21 @@ class UserTest < ActionDispatch::IntegrationTest
     assert userB.currently_registered_to(:event=>eventA)
   end
 
-  # test "invalid account" do
-  #   assert_raise (Exception) {
-  #     user = User.new(:password=>"ABCSDS231",:first_name=>"hello")
-  # }
-    
-  #   # no fields - should return an exception since none of the fields are filled out
-  # end
-  # test "edit account" do
-  #     user = User.new(:password=>"ABCSDS231",:email=>"ssekar+00000@gmu.edu",:first_name=>"hello",:last_name=>"cry",:password_confirmation=>"ABCSDS231",:admin=>false)
-  #     assert user.valid?
-  # end
-  #  test "inv reg wrong email" do
-  #   user = User.new(:password=>"ABCSDS231",:email=>"ssekar+00000@gmail.com",:first_name=>"hello",:last_name=>"crazy",:password_confirmation=>"ABCSDS231",:admin=>false)
-  #   assert user.valid?
-  # end
-  # test "inv reg mismatch password" do
-  #   user = User.new(:password=>"ABCSDS231",:email=>"ssekar+00000@gmu.edu",:first_name=>"hello",:last_name=>"crazy",:password_confirmation=>"ABCSDS234234231",:admin=>false)
-  #   assert user.valid?
-  # end
-  # test "user exists" do
-  #   user = User.new(:password=>"ABCSDS231",:email=>"ssekar+00000@gmu.edu",:first_name=>"hello",:last_name=>"crazy",:password_confirmation=>"ABCSDS231",:admin=>false)
-     
-  #   user2 = User.find(:email => "ssekar+0000.gmu.edu")
-  #   assert user==user2
-  # end
-  # test "valid login" do
-  #   user = User.new(:password=>"ABCSDS231",:email=>"ssekar+00000@gmu.edu",:first_name=>"hello",:last_name=>"crazy",:password_confirmation=>"ABCSDS231",:admin=>false)
-     
-  #   username = "ssekar+0000@gmu.edu"
-  #   password = "ABCSDS231"
-  #   boolean = (user.email == username) and (user.password == password)
-  #   assert boolean
-  # end
-  # test "invalid login" do
-  #   user = User.new(:password=>"ABCSDS231",:email=>"ssekar+00000@gmu.edu",:first_name=>"hello",:last_name=>"crazy",:password_confirmation=>"ABCSDS231",:admin=>false)
-     
-  #   username = "ssekar+0000@gmu.edu"
-  #   password = "ABCSDS2323421"
-  #   boolean = (user.email == username) and (user.password == password)
-  #   assert_not boolean
-  # end
-  # test "logged in" do
-    
-  # end
+  test "User can't join a deleted event" do
+    userA = users(:userValidA)
+    userB = users(:userValidB)
 
-  # test "logged out" do
-    
-  # end
+    post events_path, params: { event: { user_id: userA.id, event_name: 'Main Event', event_description: 'Main Event Description', event_start_date: Date.tomorrow(), event_start_time: Time.now }}
+    assert_response :redirect
+    follow_redirect!
 
-  # test "remembered" do
-    
-  # end
-  # test "is admin" do
-  #   user = User.new(:password=>"ABCSDS231",:email=>"ssekar+00000@gmu.edu",:first_name=>"hello",:last_name=>"crazy",:password_confirmation=>"ABCSDS231",:admin=>false)
-     
-  #   assert user.is_admin == false
-  # end
-  
-  # test "delete user" do
-  #   user = User.new(:password=>"ABCSDS231",:email=>"ssekar+00000@gmu.edu",:first_name=>"hello",:last_name=>"crazy",:password_confirmation=>"ABCSDS231",:admin=>false)
-     
-  #   # Need to write assertion
-  # end
-  # test "update user" do
+    eventA = Event.last
 
-  # end 
+    delete event_path(eventA)
+
+    sign_out userA
+    sign_in userB
+
+    assert_raises(Exception) { post event_user_index_url, params: { user_id: userB.id, event_user: { user_id: userB.id, event_id: eventA.id }} }
+  end
 end
